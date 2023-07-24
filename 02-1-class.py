62a67ff419ad38a8cf146ac682f6e392f1998960
zhanglongfei <zhanglongfei@cmss.chinamobile.com>
2023-07-24 19:21:15 +0800

    7.24

--- 02-1-class.py
+++
@@ -1,9 +0,0 @@
-class DEMO:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        print(self.__class__)
-        print(self.__class__.__name__)
-
-
-zhang = DEMO('zz', 18)
