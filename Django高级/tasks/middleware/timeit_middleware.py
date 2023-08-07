9ba3379a56d1a51c5e4a25496582c387c4a49a9f
zhanglongfei <zhanglongfei@cmss.chinamobile.com>
2023-08-07 18:59:30 +0800

    8-7

---
+++ Django高级/tasks/middleware/timeit_middleware.py
@@ -0,0 +1,29 @@
+import time
+from django.shortcuts import redirect
+from django.conf import settings
+
+
+def timeit_middleware(get_response):
+    def middleware(request):
+        start = time.time()
+        response = get_response(request)
+        end = time.time()
+        print("请求花费时间: {}秒".format(end - start))
+        return response
+
+    return middleware
+
+
+class LoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.login_url = settings.LOGIN_URL
+        # 开放白名单，比如['/login/', '/admin/']
+        self.open_urls = [self.login_url] + getattr(settings, 'OPEN_URLS', [])
+
+    def __call__(self, request):
+        if not request.user.is_authenticated and request.path_info not in self.open_urls:
+            return redirect(self.login_url + '?next=' + request.get_full_path())
+
+        response = self.get_response(request)
+        return response
---
+++ Django高级/tasks/signals.py
@@ -0,0 +1,9 @@
+from django.dispatch import Signal, receiver
+
+
+my_signal = Signal()
+
+
+@receiver(my_signal)
+def my_signal_callback(sender, **kwargs):
+    print(kwargs['msg']) #  打印Hello world!
