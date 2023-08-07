7a86e0afb248c6048fc0a969f297a7a565822485
zhang <1527732688@qq.com>
2023-08-07 08:57:08 +0800

    8-7

---
+++ Django_demo/dj_demo_1/app01/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('zhang')
+
+
+def user_list(request):
+    return render(request, "user_list.html")
+
+
+def user_add(request):
+    return render(request, "user_add.html")
